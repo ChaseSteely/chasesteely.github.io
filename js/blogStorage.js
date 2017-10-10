19b1d20019b8e278fd103b3108cd9a3b5e630742
Chase Steely <chasesteely@gmail.com>
2017-10-10 08:44:56 -0500

    corrected blog local storage, I think

--- js/blogStorage.js
+++ js/blogStorage.js
@@ -1,9 +1,17 @@
-let Blog = {
-    "title": Project,
+let blog = {
+    "title": "Project",
     "author": "Chase Steely",
     "description": "Description of project",
     "content": "Content of project"
 }
 
+let blogs = []
+
+blogs.push(blog)
+
+let Blog = {
+    "blog": blogs
+}
+
 const blogString = JSON.stringify(Blog)
-localStorage.setItem("blog", blogString)
+localStorage.setItem("blogStore", blogString)
