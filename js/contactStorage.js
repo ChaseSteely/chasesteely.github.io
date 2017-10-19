6d164cca5927fb4224b11ad3c7132f4def8693d8
Chase Steely <chasesteely@gmail.com>
2017-10-19 14:56:33 -0500

    started the contactDataase

--- js/contactStorage.js
+++ js/contactStorage.js
@@ -1,8 +1,46 @@
-let ContactInformation = {
-    "email": chaseteely.com,
-    "phone": 615-294-2338,
-    "location": Nashville, TN
+let contactInformation = {
+    "website": "<i class='fa fa-desktop pointer button' aria-hidden='true'></i> chaseteely.com",
+    "email": "<i class='fa fa-envelope pointer button' aria-hidden='true'></i> chaseteely@gmail.com",
+    "linkedIn": "<a href='https://www.linkedin.com/in/chasesteely/' target='_blank'><i class='fa fa-linkedin pointer button'></i></a>",
+    "github":"<a href='https://github.com/ChaseSteely'target='_blank'><i class='fa fa-github pointer button right'></i></a>",
+    "flickr": "<a href='https://www.flickr.com/photos/mchasesteely/' target='_blank'><i class='fa fa-flickr pointer button right'></i></a>",
+    "phone": "615-294-2338",
+    "location": "<i class='fa fa-phone pointer button' aria-hidden='true'></i> Nashville, TN"
 }
 
-const contactInformationString = JSON.stringify(ContactInformation)
+let contact = []
+
+contact.push(contactInformation)
+
+let ContactData = {
+    "contact": contact
+}
+
+const contactInformationString = JSON.stringify(ContactData)
 localStorage.setItem("contactInformation", contactInformationString)
+
+const ContactDatabase = JSON.parse(localStorage.getItem("contactInformation"));
+const contactElement = document.getElementsByClassName("contactInfo")[0];
+
+// Loop over keys in the database
+for (let key in ContactDatabase) {
+    // Get a reference to the array of objects
+    const thisContact = ContactDatabase[key]
+
+    // Loop over the array of objects
+    for (let i = 0; i < thisContact.length; i++) {
+        // Current item in the array
+        let item = thisContact[i]
+
+        contactElement.innerHTML += `
+                <p>"${item.website}"</p>
+                <p>"${item.email}"</p>
+                <p>"${item.linkedIn}"</p>
+                <p>"${item.github}"</p>
+                <p>"${item.flickr}"</p>
+                <p>"${item.phone}"</p>
+                <p>"${item.location}"</p>
+               
+        `
+    }//end for loop
+}//end For In loop
